8eee6a311e32606b6f322125c7bb076d13930b6a
alisofyansaputra <Buffware4@gmail.com>
2024-03-03 15:25:53 +0700

    problem4.main.py

--- problem4/main.py
+++ problem4/main.py
@@ -1,5 +1,13 @@
 def muncul_sekali(angka):
-    return []
+    count_dict = {}
+    for digit in angka:
+        if digit in count_dict:
+            count_dict[digit] += 1
+        else:
+            count_dict[digit] = 1
+    unique_digits = [int(digit) for digit, count in count_dict.items() if count == 1]
+
+    return unique_digits
 
 if __name__ == '__main__':
     print(muncul_sekali("1234123")) # [4]
