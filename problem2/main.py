a335fa1bc7d1dd0f86a80ab936cd88690aaf6833
alisofyansaputra <Buffware4@gmail.com>
2024-03-03 15:12:13 +0700

    problem2.main.py

--- problem2/main.py
+++ problem2/main.py
@@ -1,6 +1,12 @@
 def pow(x, n):
-    return 0
-
+    if n == 0:
+        return 1
+    elif n % 2 == 0:
+        half_pow = pow(x, n // 2)
+        return half_pow * half_pow
+    else:
+        half_pow = pow(x, (n - 1) // 2)
+        return x * half_pow * half_pow
 if __name__ == '__main__':
     print(pow(2, 3)) # 8
     print(pow(7, 2)) # 49
